2212e25270e51965fb397d0b7b0c9534c31b0e60
cyciling <1377899390@qq.com>
2024-09-10 12:31:04 +0800

    push theme nav

--- .vitepress/theme/components/MNavLink.vue
+++ .vitepress/theme/components/MNavLink.vue
@@ -1,163 +1,188 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import { withBase } from 'vitepress'
+import { withBase, useData } from 'vitepress'
 import { slugify } from '@mdit-vue/shared'
 
-import type { NavLink } from '../../utils/type'
+import type { NavLink, NavIcon, NavThemeIcon } from '../../utils/type'
+
+const { isDark } = useData()
 
 const props = defineProps<{
-  noIcon?: boolean
-  icon?: NavLink['icon']
-  badge?: NavLink['badge']
-  title?: NavLink['title']
-  desc?: NavLink['desc']
-  link: NavLink['link']
+    noIcon?: boolean
+    icon?: NavLink['icon']
+    badge?: NavLink['badge']
+    title?: NavLink['title']
+    desc?: NavLink['desc']
+    link: NavLink['link']
 }>()
 
+const themeIcon = (icon: NavIcon | NavThemeIcon): NavIcon => {
+    if (typeof icon === 'object' && 'dark' in icon && 'light' in icon) {
+        return isDark.value ? icon.dark : icon.light
+    }
+    // 如果是 NavIcon，直接返回
+    return icon
+}
+
 const formatTitle = computed(() => {
-  if (!props.title) {
-    return ''
-  }
-  return slugify(props.title)
+    if (!props.title) {
+        return ''
+    }
+    return slugify(props.title)
 })
 
 const svg = computed(() => {
-  if (typeof props.icon === 'object') return props.icon.svg
-  return ''
+    if(props.icon){
+        const icon = themeIcon(props.icon)
+        if (typeof icon === 'object' && 'svg' in icon) {
+            return icon.svg
+        }
+    }
+    return ''
+})
+
+const url = computed(() => {
+    if(props.icon){
+        const icon = themeIcon(props.icon)
+        if (typeof icon === 'string') {
+            return withBase(icon)
+        }
+    }
+    return ''
 })
 
 const formatBadge = computed(() => {
-  if (typeof props.badge === 'string') {
-    return { text: props.badge, type: 'info' }
-  }
-  return props.badge
+    if (typeof props.badge === 'string') {
+        return { text: props.badge, type: 'info' }
+    }
+    return props.badge
 })
 </script>
 
 <template>
-  <a v-if="link" class="m-nav-link" :href="link" target="_blank" rel="noreferrer">
-    <article class="box" :class="{ 'has-badge': formatBadge }">
-      <div class="box-header">
-        <template v-if="!noIcon">
-          <div v-if="svg" class="icon" v-html="svg"></div>
-          <div v-else-if="icon && typeof icon === 'string'" class="icon">
-            <img
-              :src="withBase(icon)"
-              :alt="title"
-              onerror="this.parentElement.style.display='none'"
-            />
-          </div>
-        </template>
-        <h5 v-if="title" :id="formatTitle" class="title" :class="{ 'no-icon': noIcon }">
-          {{ title }}
-        </h5>
-      </div>
-      <Badge v-if="formatBadge" class="badge" :type="formatBadge.type" :text="formatBadge.text" />
-      <p v-if="desc" class="desc">{{ desc }}</p>
-    </article>
-  </a>
+    <a v-if="link" class="m-nav-link" :href="link" target="_blank" rel="noreferrer">
+        <article class="box" :class="{ 'has-badge': formatBadge }">
+            <div class="box-header">
+                <template v-if="!noIcon">
+                    <div v-if="svg" class="icon" v-html="svg"></div>
+                    <div v-else-if="url" class="icon">
+                        <img
+                            :src="url"
+                            :alt="title"
+                            onerror="this.parentElement.style.display='none'"
+                        />
+                    </div>
+                </template>
+                <h5 v-if="title" :id="formatTitle" class="title" :class="{ 'no-icon': noIcon }">
+                    {{ title }}
+                </h5>
+            </div>
+            <Badge v-if="formatBadge" class="badge" :type="formatBadge.type" :text="formatBadge.text" />
+            <p v-if="desc" class="desc">{{ desc }}</p>
+        </article>
+    </a>
 </template>
 
 <style lang="scss" scoped>
 .m-nav-link {
-  --m-nav-icon-box-size: 50px;
-  --m-nav-icon-size: 45px;
-  --m-nav-box-gap: 12px;
+    --m-nav-icon-box-size: 50px;
+    --m-nav-icon-size: 45px;
+    --m-nav-box-gap: 12px;
 
-  display: block;
-  border: 1px solid var(--vp-c-bg-soft);
-  border-radius: 12px;
-  height: 100%;
-  background-color: var(--vp-c-bg-soft);
-  transition: all 0.25s;
-
-  &:hover {
-    box-shadow: var(--vp-shadow-2);
-    border-color: var(--vp-c-brand);
-    text-decoration: initial;
-    background-color: var(--vp-c-bg-soft-up);
-    transform: translateY(-5px);
-  }
-
-  .box {
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    padding: var(--m-nav-box-gap);
+    display: block;
+    border: 1px solid var(--vp-c-bg-soft);
+    border-radius: 12px;
     height: 100%;
-    color: var(--vp-c-text-1);
-    &.has-badge {
-      padding-top: calc(var(--m-nav-box-gap) + 2px);
-    }
-    &-header {
-      display: flex;
-      align-items: center;
-    }
-  }
+    background-color: var(--vp-c-bg-soft);
+    transition: all 0.25s;
 
-  .icon {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-right: calc(var(--m-nav-box-gap) - 2px);
-    border-radius: 6px;
-    width: var(--m-nav-icon-box-size);
-    height: var(--m-nav-icon-box-size);
-    font-size: var(--m-nav-icon-size);
-    background-color: var(--vp-c-bg-soft-down);
-    transition: background-color 0.25s;
-    :deep(svg) {
-      width: var(--m-nav-icon-size);
-      fill: currentColor;
+    &:hover {
+        box-shadow: var(--vp-shadow-2);
+        border-color: var(--vp-c-brand);
+        text-decoration: initial;
+        background-color: var(--vp-c-bg-soft-up);
+        transform: translateY(-5px);
     }
-    :deep(img) {
-      border-radius: 4px;
-      width: var(--m-nav-icon-size);
+
+    .box {
+        display: flex;
+        flex-direction: column;
+        position: relative;
+        padding: var(--m-nav-box-gap);
+        height: 100%;
+        color: var(--vp-c-text-1);
+        &.has-badge {
+            padding-top: calc(var(--m-nav-box-gap) + 2px);
+        }
+        &-header {
+            display: flex;
+            align-items: center;
+        }
     }
-  }
 
-  .title {
-    overflow: hidden;
-    flex-grow: 1;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    font-size: 16px;
-    font-weight: 600;
-    &:not(.no-icon) {
-      line-height: var(--m-nav-icon-box-size);
+    .icon {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-right: calc(var(--m-nav-box-gap) - 2px);
+        border-radius: 6px;
+        width: var(--m-nav-icon-box-size);
+        height: var(--m-nav-icon-box-size);
+        font-size: var(--m-nav-icon-size);
+        background-color: var(--vp-c-bg-soft-down);
+        transition: background-color 0.25s;
+        :deep(svg) {
+            width: var(--m-nav-icon-size);
+            fill: currentColor;
+        }
+        :deep(img) {
+            border-radius: 4px;
+            width: var(--m-nav-icon-size);
+        }
     }
-  }
 
-  .badge {
-    position: absolute;
-    top: 2px;
-    right: 0;
-    transform: scale(0.8);
-  }
+    .title {
+        overflow: hidden;
+        flex-grow: 1;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        font-size: 16px;
+        font-weight: 600;
+        &:not(.no-icon) {
+            line-height: var(--m-nav-icon-box-size);
+        }
+    }
 
-  .desc {
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    flex-grow: 1;
-    margin: calc(var(--m-nav-box-gap) - 2px) 0 0;
-    line-height: 1.5;
-    font-size: 12px;
-    color: var(--vp-c-text-2);
-  }
+    .badge {
+        position: absolute;
+        top: 2px;
+        right: 0;
+        transform: scale(0.8);
+    }
+
+    .desc {
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        flex-grow: 1;
+        margin: calc(var(--m-nav-box-gap) - 2px) 0 0;
+        line-height: 1.5;
+        font-size: 12px;
+        color: var(--vp-c-text-2);
+    }
 }
 
 @media (max-width: 960px) {
-  .m-nav-link {
-    --m-nav-icon-box-size: 36px;
-    --m-nav-icon-size: 20px;
-    --m-nav-box-gap: 8px;
+    .m-nav-link {
+        --m-nav-icon-box-size: 36px;
+        --m-nav-icon-size: 20px;
+        --m-nav-box-gap: 8px;
 
-    .title {
-      font-size: 14px;
+        .title {
+            font-size: 14px;
+        }
     }
-  }
 }
 </style>
