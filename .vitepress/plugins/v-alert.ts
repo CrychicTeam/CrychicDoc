1032e3e3df3810ed59823a608cdc04e2d5e5a5d9
cyciling <1377899390@qq.com>
2024-09-10 14:46:06 +0800

    fix <p>

--- .vitepress/plugins/v-alert.ts
+++ .vitepress/plugins/v-alert.ts
@@ -1,4 +1,5 @@
 import { container } from "@mdit/plugin-container";
+import { logger } from '../config/sidebarControl';
 import type { PluginSimple } from "markdown-it";
 
 export const v_alert: PluginSimple = (md) => {
@@ -8,37 +9,15 @@ export const v_alert: PluginSimple = (md) => {
             container(md, {
                 name,
                 openRender: (tokens, index, _options) => {
-                        const info: string = tokens[index].info.trim().slice(name.length).trim();
-                        const defaultTitle: string = name.replace("v-", "")
-                        return `<p><v-alert title="${info || defaultTitle}" type="${defaultTitle}" >\n`;
+                    const info: string = tokens[index].info.trim().slice(name.length).trim();
+                    const defaultTitle: string = name.replace("v-", "")
+                    return `<p><v-alert class="v-alert" title="${info || defaultTitle}" type="${defaultTitle}" >\n`;
                 },
-                closeRender: (): string => `</v-alert></p>\n`,
+                closeRender: (): string => {
+                    return `</v-alert></p>\n`
+                },
             }),
         )
         
     );
-
-    md.renderer.rules.paragraph_open = (tokens, idx, options, env, self) => {
-        const open = tokens[idx - 1];
-        if (open && open.type.startsWith('container_v-') && open.type.endsWith('open')) {
-            return ''; // Skip paragraph opening inside container
-        }
-        const close = tokens[idx + 3];
-        if (close && close.type.startsWith('container_v-') && close.type.endsWith('close')) {
-            return '\n'; // Skip paragraph closing inside container
-        }
-        return self.renderToken(tokens, idx, options);
-    };
-
-    md.renderer.rules.paragraph_close = (tokens, idx, options, env, self) => {
-        const close = tokens[idx + 1];
-        if (close && close.type.startsWith('container_v-') && close.type.endsWith('close')) {
-            return '\n'; // Skip paragraph closing inside container
-        }
-        const open = tokens[idx - 3];
-        if (open && open.type.startsWith('container_v-') && open.type.endsWith('open')) {
-            return ''; // Skip paragraph opening inside container
-        }
-        return self.renderToken(tokens, idx, options);
-    };
 };
