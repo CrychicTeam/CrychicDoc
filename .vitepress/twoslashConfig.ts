08e34a1160f88cff3fa3dd360d0f51463868578e
M1hono <xche0557@uni.sydney.edu.au>
2024-09-16 14:30:20 +1000

    Test

--- .vitepress/twoslashConfig.ts
+++ .vitepress/twoslashConfig.ts
@@ -2,14 +2,7 @@ import { cwd } from "node:process";
 import { join } from "node:path";
 import fs from "fs";
 
-const typeFilesPath = join(
-    cwd(),
-    "typefiles",
-    "1.20.1",
-    "probe",
-    "generated",
-    "internals"
-);
+const typeFilesPath = join(cwd(), "typefiles/1.20.1/probe/generated/internals");
 const internalTypeFiles = fs.existsSync(typeFilesPath)
     ? fs
           .readdirSync(typeFilesPath)
@@ -19,22 +12,15 @@ const internalTypeFiles = fs.existsSync(typeFilesPath)
           .map((file) => join(typeFilesPath, file))
     : [];
 
-// 添加一个日志，以便当目录不存在时能够进行排查
-if (!internalTypeFiles.length) {
-    console.warn(`No internal type files found in ${typeFilesPath}`);
-}
-
 export const compilerOptions = {
     cache: true,
     compilerOptions: {
         baseUrl: cwd(),
-        target: "ES2020", // 使用更标准的 TypeScript 目标
-        module: "ESNext", // 使用标准模块格式
-        moduleResolution: "node",
+        target: 99,
+        module: 99,
+        moduleResolution: 100,
         paths: {
-            "*": [
-                join(cwd(), "typefiles", "1.20.1", "probe", "generated", "*"),
-            ],
+            "*": [join(cwd(), "typefiles/1.20.1/probe/generated/*")],
         },
         resolveJsonModule: true,
         types: ["node", ...internalTypeFiles], // 自动使用文件，如果没有则保持现状
